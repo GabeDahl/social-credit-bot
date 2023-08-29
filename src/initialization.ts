7ba706f906ef17cefb6a8353435aa17ad5bc6e30
Gabriel <gabrdahl@gmail.com>
2023-08-29 11:03:58 -0500

    syncUsers() on bot startup instead of when joining new server

--- src/initialization.ts
+++ src/initialization.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { initDiscordClient } from './services/discord/init.js';
 import { NlpMachine } from './models/message-analysis/NlpMachine.js';
 import { messageHandler } from './services/discord/events/message-handler.js';
-import { Client, Events, Guild } from 'discord.js';
+import { Client, Events, GuildMemberManager } from 'discord.js';
 import { positiveStatements } from './services/message-analysis/training-data/positive.js';
 import { TrainingDocument } from './models/message-analysis/TrainingDocument.js';
 import { negativeStatements } from './services/message-analysis/training-data/negative.js';
@@ -11,13 +11,12 @@ import { syncUsers } from './services/firebase/users.js';
 import { interactionHandler } from './services/discord/events/interaction.js';
 
 export const startBot = async (client: Client, nlpMachine: NlpMachine) => {
-  client.once(Events.GuildCreate, async ({ members, available }: Guild | Partial<Guild>) => {
-    if (available === false || !members) return;
-    await members.fetch();
-    const nonBotMembers = members.cache.filter((member) => !member.user.bot);
-    const nonBotIds = nonBotMembers.map((member) => member.id);
-    if (nonBotIds.length === 0) return;
-    await syncUsers(nonBotIds);
+  client.once(Events.ClientReady, async () => {
+    for (const guild of client.guilds.cache.values()) {
+      if (guild.available) {
+        await fetchAndSyncUsers(guild.members);
+      }
+    }
   });
   client.addListener(Events.MessageCreate, (msg) => messageHandler(msg, client, nlpMachine));
   client.addListener(Events.InteractionCreate, (interaction) => interactionHandler(interaction));
@@ -37,3 +36,11 @@ export const trainMachine = async (nlpMachine: NlpMachine) => {
   nlpMachine.loadData(docs);
   return await nlpMachine.train();
 };
+
+async function fetchAndSyncUsers(members: GuildMemberManager) {
+  await members.fetch();
+  const nonBotMembers = members.cache.filter((member) => !member.user.bot);
+  const nonBotIds = nonBotMembers.map((member) => member.id);
+  if (nonBotIds.length === 0) return;
+  await syncUsers(nonBotIds);
+}
