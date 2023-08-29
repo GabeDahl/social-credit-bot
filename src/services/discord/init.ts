cf954b3f5db44ad1354fe2436cdee21d8fc9d0e8
Gabriel <gabrdahl@gmail.com>
2023-08-29 10:51:22 -0500

    man that's some less than ideal documentation

--- src/services/discord/init.ts
+++ src/services/discord/init.ts
@@ -4,7 +4,7 @@ import { Client } from 'discord.js';
 export const initDiscordClient = async (client: Client) => {
   const secretMgrClient = new SecretManagerServiceClient();
   const [accessResponse] = await secretMgrClient.accessSecretVersion({
-    name: 'projects/808553059840/secrets/DISCORD_TOKEN',
+    name: 'projects/808553059840/secrets/DISCORD_TOKEN/versions/latest',
   });
   const token = accessResponse.payload?.data?.toString();
   if (!token) {
